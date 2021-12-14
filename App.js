c2ff47a20b8da2616a3d4a498f477d07b976e90d
jadelcruz1 <jardelcruz6@gmail.com>
2021-12-14 10:19:00 -0300

    Criando o compenent Title

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Title from './src/components/title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App de cotação de Crypto moedas! Jardel</Text>
-      <StatusBar style="auto" />
+
+      <Title/>
+
     </View>
   );
 }
---
+++ src/components/title/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export default function Title (){
+    return( 
+        <View>
+            <Text> Health's Plan</Text>
+            <Text> Aplicativo para a sua saúde. </Text>
+
+        </View>
+    );
+}
